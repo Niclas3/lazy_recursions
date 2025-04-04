9764f227a1c5d18fec0072af355fb16dabbe056b
zm <missingurchin@gmail.com>
2025-04-04 23:54:00 +0800

    feat: a React component for mdx posts <More_info>

---
+++ app/ui/More_info.tsx
@@ -0,0 +1,7 @@
+export default function More_info({children}: {children: React.ReactNode }){
+    return (
+        <div className="bg-[#d0e7f9] text-[#4d4d4d] rounded-md p-3">
+        {children}
+        </div>
+    );
+}
