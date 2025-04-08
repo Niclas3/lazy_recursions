51db9a6dfa33e197bd7243a6cc82ddbd2c711101
zm <missingurchin@gmail.com>
2025-04-08 17:02:39 +0800

    feat(mdx_custom_component): More_info now can support MD formatting

--- app/ui/More_info.tsx
+++ app/ui/More_info.tsx
@@ -1,7 +1,10 @@
+import { CustomMDX } from '@/app/ui/CustomMDX'
+
 export default function More_info({children}: {children: React.ReactNode }){
     return (
         <div className="bg-[#d0e7f9] text-[#4d4d4d] rounded-md p-3">
-        {children}
+               <CustomMDX source={children}>
+               </CustomMDX>
         </div>
     );
 }
