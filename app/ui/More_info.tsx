593b9255cd17be3e8ab719fccf15094c0ced972c
zm <missingurchin@gmail.com>
2025-04-10 18:03:03 +0800

    feat(MDX_components): add bottom margin to <More_info>

--- app/ui/More_info.tsx
+++ app/ui/More_info.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function More_info({children, cite}: {children: React.ReactNode, cite: string }){
         const icon = <BookOpen />;
         return (
-                <Blockquote color="indigo" radius="lg" cite={ cite } icon={icon} mt="xl">
+                <Blockquote color="indigo" radius="lg" cite={ cite } icon={icon} mt="xl" mb="xl">
                         <div className="text-base not-italic not-prose">
                                 {children}
                         </div>
