50e9f2318302a6835388ba4cb61757e1be2f0b88
zm <missingurchin@gmail.com>
2025-04-10 15:28:33 +0800

    feat(MDX_component): Add <Expanded_info/>
    For expanding a large text

---
+++ app/ui/Expanded_info.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { Collapse, Text, Box, Group, Button } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import React from 'react'
+
+export default function Expanded_info({children}:{children: React.ReactNode}) {
+        const [opened, {toggle}] = useDisclosure(false)
+        return (
+                <Box>
+                        <Group justify="center" mb={5}>
+                                <Button onClick={toggle}>
+                                        seeeeeee
+                                </Button>
+                        </Group>
+                        <Collapse in={opened}>
+                                <Text>
+                                        {children}
+                                </Text>
+                        </Collapse>
+
+                </Box>
+        );
+}
