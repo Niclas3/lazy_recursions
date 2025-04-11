959786f3bdf3dc6b67f25565eb281456b4c3094b
zm <missingurchin@gmail.com>
2025-04-11 17:46:32 +0800

    chore: remove useless code

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ import Home_page from '@/app/home_page'
 export default async function Personal() {
    const posts = allPosts
 
-   posts.forEach((post)=>{
-           console.log(post.url)
-   })
    // Some weird here this variable `meta` suppose to be a Array<BlogPost> but
    // shows an object.
    return (<Home_page blog_posts = {posts} projects={PROJECTS} /> )
