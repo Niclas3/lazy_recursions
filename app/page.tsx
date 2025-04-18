af9213fd3ff6dbf7cb168b81102ff805842fcd98
zm <missingurchin@gmail.com>
2025-04-18 23:13:06 +0800

    feat(main_page): sort posts by date

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import {PROJECTS} from './data'
 
-import { allPosts } from 'contentlayer/generated'
+import { allPosts, Post } from 'contentlayer/generated'
 
 import Home_page from '@/app/home_page'
 
 export default async function Personal() {
-   const posts = allPosts
+
+   const posts = allPosts.filter((value: Post)=> !value.draft)
+                         .sort((a:Post, b:Post)=>{
+                                const a_date = a?.date;
+                                const b_date = b?.date;
+                                if(a_date && b_date){
+                                        const ta = new Date(a_date)
+                                        const tb = new Date(b_date)
+                                        return ta > tb ? -1 : 1
+                                } else {
+                                        return 0
+                        }})
 
    // Some weird here this variable `meta` suppose to be a Array<BlogPost> but
    // shows an object.
