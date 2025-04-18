2d4a6fd910a1b9fd79b3db8287724281e8f63c57
zm <missingurchin@gmail.com>
2025-04-18 14:12:45 +0800

    feat(MDX_components): add `name` props in <Expanded_info/>
    change some css style

--- app/ui/Expanded_info.tsx
+++ app/ui/Expanded_info.tsx
@@ -1,15 +1,15 @@
 "use client"
-import { Collapse, Text, Box, Group, Button } from '@mantine/core'
+import { Collapse,  Box, Group, Button } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import React from 'react'
 
-export default function Expanded_info({children}:{children: React.ReactNode}) {
+export default function Expanded_info({name="more", children}:{name: string ,children: React.ReactNode}) {
         const [opened, {toggle}] = useDisclosure(false)
         return (
-                <Box>
-                        <Group justify="center" mb={5}>
+                <Box className="mb-4">
+                        <Group justify="left" mb={5}>
                                 <Button onClick={toggle}>
-                                        seeeeeee
+                                        {name}
                                 </Button>
                         </Group>
                         <Collapse in={opened}>
