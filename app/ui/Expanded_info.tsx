f2f8a1353421efec4124d2805b14eb0d2aeee0bc
zm <missingurchin@gmail.com>
2025-04-21 13:25:07 +0800

    feat(MDX_components): add background-color on <Expanded_info/>

--- app/ui/Expanded_info.tsx
+++ app/ui/Expanded_info.tsx
@@ -6,16 +6,16 @@ import React from 'react'
 export default function Expanded_info({name="more", children}:{name: string ,children: React.ReactNode}) {
         const [opened, {toggle}] = useDisclosure(false)
         return (
-                <Box className="mb-4">
+                <Box className="mb-4 ">
                         <Group justify="left" mb={5}>
                                 <Button onClick={toggle}>
                                         {name}
                                 </Button>
                         </Group>
-                        <Collapse in={opened}>
+                        <Collapse in={opened} className="bg-gray-100">
                                 {children}
                         </Collapse>
-
+                        <hr/>
                 </Box>
         );
 }
